e70357c1e4150f8618af1702766fe90de1023d4d
shrey200634 <dshrey60@gmail.com>
2026-05-13 16:31:07 +0530

    -Add Redisson config in api service -add depedency in build.gradke - complete yml file in the application . properties

---
+++ Backend/api-service/src/main/java/com/distributedjobforge/api_service/config/RedissonConfig.java
@@ -0,0 +1,32 @@
+package com.distributedjobforge.api_service.config;
+
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Value("${redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${redis.port:6379}")
+    private int redisPort;
+
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + redisHost + ":" + redisPort)
+                .setConnectionMinimumIdleSize(5)
+                .setConnectionPoolSize(10)
+                .setTimeout(3000)
+                .setRetryAttempts(3)
+                .setRetryInterval(1500);
+        return Redisson.create(config);
+    }
+}
