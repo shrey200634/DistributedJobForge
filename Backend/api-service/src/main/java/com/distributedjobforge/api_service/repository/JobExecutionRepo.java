465c9c91d5704956ddb5d832fc4a657b6339faf6
shrey200634 <dshrey60@gmail.com>
2026-05-13 17:38:26 +0530

    added repository in the api service

--- Backend/api-service/src/main/java/com/distributedjobforge/api_service/repository/JobExecutionRepo.java
+++ Backend/api-service/src/main/java/com/distributedjobforge/api_service/repository/JobExecutionRepo.java
@@ -1,4 +1,17 @@
 package com.distributedjobforge.api_service.repository;
 
-public class JobExecutionRepo {
+import com.distributedjobforge.api_service.domain.JobExecution;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+import java.util.*;
+
+
+
+@Repository
+public interface  JobExecutionRepo extends JpaRepository<JobExecution , UUID> {
+
+    List<JobExecution>  findByJobIdOrderByAttemptAsc(UUID jobId );
+
 }
--- Backend/api-service/src/main/java/com/distributedjobforge/api_service/repository/JobRepo.java
+++ Backend/api-service/src/main/java/com/distributedjobforge/api_service/repository/JobRepo.java
@@ -1,4 +1,24 @@
 package com.distributedjobforge.api_service.repository;
 
-public class JobRepo {
+import com.distributedjobforge.api_service.domain.Job;
+import com.distributedjobforge.api_service.domain.JobStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+
+import java.util.UUID;
+
+
+@Repository
+public interface  JobRepo   extends JpaRepository<Job , UUID> {
+
+    List<Job> findByStatus(JobStatus status);
+
+    Page<Job> findByStatus (JobStatus status , Pageable pageable);
+
+    List<Job> findByWorkerId(String workerId );
 }
