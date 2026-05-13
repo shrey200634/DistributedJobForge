ff41e50f5694040216c3d3ea493d706ba4d26895
shrey200634 <dshrey60@gmail.com>
2026-05-13 17:25:54 +0530

    add Job execution database in domain into the api service

--- Backend/api-service/src/main/java/com/distributedjobforge/api_service/domain/JobExecution.java
+++ Backend/api-service/src/main/java/com/distributedjobforge/api_service/domain/JobExecution.java
@@ -1,4 +1,50 @@
 package com.distributedjobforge.api_service.domain;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Table(name = "job_execution")
+
 public class JobExecution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id ;
+
+    private int attempt ;
+
+    @Column(name = "worker_id")
+    private String workerId ;
+
+    @Column(name = "started_at")
+    private Instant startedAt ;
+
+    @Column(name = "end_at")
+    private Instant endAt ;
+
+    @Column(name = "exit_code")
+    private Integer exitCode ;
+
+    @Column(columnDefinition = "TEXT")
+    private String stdout ;
+
+    @Column (columnDefinition = "TEXT")
+    private String stderr;
+
+    @Column (name = "duration_ms ")
+    private Long durationMs ;
+
+    @Enumerated(EnumType.STRING)
+    private JobStatus status ;
+
 }
