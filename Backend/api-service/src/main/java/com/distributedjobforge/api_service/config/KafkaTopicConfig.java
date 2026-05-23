7be7fd43ddbff89d3710fb96be36eb9045084dfe
shrey200634 <dshrey60@gmail.com>
2026-05-23 16:29:59 +0530

    - add docker file to the api service - edit docker compose yml at root dir - complete kafka config at the config package in api service -image build >> and
    now able to submit job it generate the uuid and give status pending and submit it to the kafka topic ehich is job.pending

---
+++ Backend/api-service/src/main/java/com/distributedjobforge/api_service/config/KafkaTopicConfig.java
@@ -0,0 +1,35 @@
+package com.distributedjobforge.api_service.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic jobPendingTopic (){
+        return TopicBuilder.name("job.pending")
+                .partitions(12)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public  NewTopic jobResultTopic(){
+        return TopicBuilder.name("job.result")
+                .partitions(6)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public  NewTopic jobDlqTopic(){
+        return TopicBuilder.name("job.dlq")
+                .partitions(3)
+                .replicas(1)
+                .build();
+
+    }
+}
