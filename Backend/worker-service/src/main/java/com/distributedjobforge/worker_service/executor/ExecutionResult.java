d1e25f54e561e1b30244ce6d647c19bbc70af425
shrey200634 <dshrey60@gmail.com>
2026-05-28 14:25:34 +0530

    add rhe executor result in worker service in executor section

--- Backend/worker-service/src/main/java/com/distributedjobforge/worker_service/executor/ExecutionResult.java
+++ Backend/worker-service/src/main/java/com/distributedjobforge/worker_service/executor/ExecutionResult.java
@@ -1,4 +1,43 @@
 package com.distributedjobforge.worker_service.executor;
 
-public class ExecutionResult {
+import com.distributedjobforge.worker_service.domain.JobStatus;
+import org.apache.kafka.common.protocol.types.Field;
+
+import java.time.Instant;
+
+public record ExecutionResult (
+
+        JobStatus status,
+        Integer exitCode,
+        String stdout,
+        String stderr,
+        String errorMessage,
+        Instant startedAt,
+        Instant completedAt,
+        long durationMs
+)
+{
+    public  static ExecutionResult success (int exitCode , String stdout , String stderr,
+                                            Instant startedAt , Instant completedAt ){
+        return  new ExecutionResult(
+                JobStatus.SUCCEEDED , exitCode , stdout , stderr , null ,
+                startedAt, completedAt , completedAt.toEpochMilli()-startedAt.toEpochMilli()
+        );
+    }
+
+    public  static ExecutionResult failure (int exitCode , String stdout , String stderr
+                                            , String errorMessage , Instant startedAt , Instant completedAt ){
+        return new ExecutionResult(
+                JobStatus.FAILED , exitCode , stdout , stderr , errorMessage
+                ,startedAt , completedAt , completedAt.toEpochMilli() - startedAt.toEpochMilli()
+
+        );
+    }
+    public  static ExecutionResult timeout (String stdout , String stderr ,
+                                            Instant startedAt , Instant completedAt){
+        return  new ExecutionResult(
+                JobStatus.TIMEOUT , null , stdout , stderr , "process exceed timeout ",
+                startedAt , completedAt , completedAt.toEpochMilli()-startedAt.toEpochMilli()
+        );
+    }
 }
